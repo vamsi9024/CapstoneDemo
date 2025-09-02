4393c37dc1f4916dffe368ed19c7e46e79b166e9
niharikasingh9153 <singhniharika9153@gmail.com>
2025-09-02 17:13:51 +0530

    commit old

--- src/main/java/com/example/demo/model/AuthRequest.java
+++ src/main/java/com/example/demo/model/AuthRequest.java
@@ -1,7 +1,9 @@
 package com.example.demo.model;
-
+import jakarta.validation.constraints.NotBlank;
 public class AuthRequest {
+    @NotBlank(message = "Username is required")
     private String username;
+    @NotBlank(message = "Password is required")
     private String password;
     public AuthRequest() {}
 
